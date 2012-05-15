405a4302ebd97970b030430a2563c90bcbd5742c
Johannes Unterstein <unterstein@me.com>
2012-05-15 14:05:43 +0200

    added PageParameterAwareMountedMapper

---
+++ src/main/java/org/wickettales/request/mapper/PageParameterAwareMountedMapper.java
@@ -0,0 +1,83 @@
+package org.wickettales.request.mapper;
+
+import org.apache.wicket.request.IRequestHandler;
+import org.apache.wicket.request.component.IRequestablePage;
+import org.apache.wicket.request.handler.RenderPageRequestHandler;
+import org.apache.wicket.request.mapper.MountedMapper;
+import org.apache.wicket.request.mapper.info.PageInfo;
+import org.apache.wicket.request.mapper.parameter.IPageParametersEncoder;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.util.ClassProvider;
+
+/**
+ * This mapper represents exactly the same behavior like the default
+ * {@link MountedMapper}, except one behavior:<br/>
+ * The default {@link MountedMapper} implementation prefers the pageId parameter
+ * with a higher priority then the rest of the {@link PageParameters}.<br>
+ * This preference leads to the following behavior:
+ * 
+ * <pre>
+ * ...myApplication/User/4?15
+ * </pre>
+ * 
+ * leads to the UserPage with the indexed {@link PageParameters} "4" at index 0.
+ * So the User with id 4 is shown. When a user manually changes the URL to
+ * something like this:
+ * 
+ * <pre>
+ * ...myApplication/User/5?15
+ * </pre>
+ * 
+ * The {@link MountedMapper} would deliver the same page as before and would
+ * show the user with id 4 and not - like it is implicated in the url - the user
+ * with id 5, because the pageId parameter is preferred during mapping process. <br/>
+ * <br/>
+ * Therefore this implementation compares the existing {@link PageParameters} of
+ * the cached page and the {@link PageParameters} of the current request. If a
+ * difference between this two {@link PageParameters} are recognized, this
+ * mapper redirects to a fresh bookmarkable instance of the current requested
+ * page.
+ * 
+ * @author <a href="mailto:unterstein@me.com">Johannes Unterstein</a>
+ * 
+ */
+public class PageParameterAwareMountedMapper extends MountedMapper {
+
+    public PageParameterAwareMountedMapper(String mountPath, Class<? extends IRequestablePage> pageClass) {
+        super(mountPath, pageClass);
+    }
+
+    public PageParameterAwareMountedMapper(String mountPath, Class<? extends IRequestablePage> pageClass,
+            IPageParametersEncoder pageParametersEncoder) {
+        super(mountPath, pageClass, pageParametersEncoder);
+    }
+
+    public PageParameterAwareMountedMapper(String mountPath, ClassProvider<? extends IRequestablePage> pageClassProvider) {
+        super(mountPath, pageClassProvider);
+    }
+
+    public PageParameterAwareMountedMapper(String mountPath, ClassProvider<? extends IRequestablePage> pageClassProvider,
+            IPageParametersEncoder pageParametersEncoder) {
+        super(mountPath, pageClassProvider, pageParametersEncoder);
+    }
+
+    @Override
+    protected IRequestHandler processHybrid(PageInfo pageInfo, Class<? extends IRequestablePage> pageClass, PageParameters pageParameters,
+            Integer renderCount) {
+        IRequestHandler handler = super.processHybrid(pageInfo, pageClass, pageParameters, renderCount);
+        if (handler instanceof RenderPageRequestHandler) {
+            // in the current implementation (wicket 1.5.6) super.processHybrid
+            // returns a RenderPageRequestHandler
+            RenderPageRequestHandler renderPageHandler = (RenderPageRequestHandler) handler;
+            PageParameters newPageParameters = renderPageHandler.getPageParameters();
+            PageParameters oldPageParameters = renderPageHandler.getPageProvider().getPageInstance().getPageParameters();
+            // if we recognize a change between the page parameter of the loaded
+            // page and the page parameter of the current request, we redirect
+            // to a fresh bookmarkable instance of that page.
+            if (!PageParameters.equals(oldPageParameters, newPageParameters)) {
+                handler = processBookmarkable(pageClass, newPageParameters);
+            }
+        }
+        return handler;
+    }
+}
