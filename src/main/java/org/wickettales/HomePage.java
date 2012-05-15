dcd1487acc50b9e966ed8b841b60fffa8cb0c80e
Johannes Unterstein <unterstein@me.com>
2012-05-15 13:20:51 +0200

    test

--- src/main/java/org/wickettales/HomePage.java
+++ src/main/java/org/wickettales/HomePage.java
@@ -8,6 +8,6 @@ public class HomePage extends WebPage {
     private static final long serialVersionUID = 1L;
 
     public HomePage(final PageParameters parameters) {
-        add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+        add(new Label("version", getApplication().getFrameworkSettings().getVersion())); 
     }
 }
