bfb1e6edc1e3476174acd90e94b50f2c30c6cfff
Michael Bruns <miggel@gmx.org>
2012-05-17 22:24:29 +0200

    added javadoc and added a counter to the EventPage

--- examples/event-example/src/main/java/org/wickettales/RefreshClick.java
+++ examples/event-example/src/main/java/org/wickettales/RefreshClick.java
@@ -3,16 +3,30 @@ package org.wickettales;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 
+/**
+ * This is the payload of Events fired by a {@link RefreshLink}.
+ * 
+ * Components which shall be refreshed can be added via {@link RefreshClick#refresh(Component...)}.
+ * They will be added to the current AjaxRequestTarget.
+ * 
+ * @author Michael Bruns
+ */
 public class RefreshClick {
 
-	private final AjaxRequestTarget target;
+    private final AjaxRequestTarget target;
 
-	public RefreshClick(AjaxRequestTarget target) {
-		this.target = target;
-	}
-	
-	public void update(Component... components) {
-		target.add(components);
-	}
+    public RefreshClick(AjaxRequestTarget target) {
+        this.target = target;
+    }
+
+    /**
+     * Adds the given Components to the current AjaxRequestTarget.
+     * 
+     * @param components
+     *            the components to refresh
+     */
+    public void refresh(Component... components) {
+        target.add(components);
+    }
 
 }
--- examples/event-example/src/main/java/org/wickettales/RefreshOnClickBehavior.java
+++ examples/event-example/src/main/java/org/wickettales/RefreshOnClickBehavior.java
@@ -4,13 +4,20 @@ import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.event.IEvent;
 
+/**
+ * Handles Events which have a {@link RefreshClick} as payload. Usually they are fired by a {@link RefreshLink}.
+ * 
+ * The Components to which this behavior is added are passed to {@link RefreshClick#refresh(Component...)}.
+ * 
+ * @author Michael Bruns
+ */
 public class RefreshOnClickBehavior extends Behavior {
 
-	@Override
-	public void onEvent(Component component, IEvent<?> event) {
-		if (event.getPayload() != null && event.getPayload() instanceof RefreshClick) {
-			((RefreshClick) event.getPayload()).update(component);
-		}
-	}
+    @Override
+    public void onEvent(Component component, IEvent<?> event) {
+        if (event.getPayload() != null && event.getPayload() instanceof RefreshClick) {
+            ((RefreshClick) event.getPayload()).refresh(component);
+        }
+    }
 
 }
